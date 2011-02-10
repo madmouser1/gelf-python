98e0b1b876e3bc9ce49bda5ad668b39cc1a4c9e4
Joshua Spaulding <joshua.spaulding@gmail.com>
2011-02-10 10:48:13 -0600

    refactor

--- GELFClient.py
+++ GELFClient.py
@@ -2,8 +2,10 @@ class GELFClient:
 	VERSION = "1.0"
 	graylog_server = "127.0.0.1"
 	graylog_port = 12201 
-	maxChunkSize = "WAN"
+	networkMaxChunkSize = 1420
+	lanMaxChunkSize = 8154
 	data =
 
 	def construct(graylog_server, graylog_port, maxChunkSize = "WAN"):
+		
 		
